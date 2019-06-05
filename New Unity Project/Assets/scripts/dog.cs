ecd903ba4b5a39d560d34eb9bedbf666abc27258
jack0300501 <51389225+jack0300501@users.noreply.github.com>
2019-06-05 13:12:47 +0800

    狗狗吃東西的方法

--- New Unity Project/Assets/scripts/dog.cs
+++ New Unity Project/Assets/scripts/dog.cs
@@ -12,9 +12,37 @@ public class dog
     #endregion
 
     #region 方法;
+    /// <summary>
+    /// 狗狗亂叫的方法
+    /// </summary>
     public void Bark()
     {
         Debug.Log(name + "旺旺!!");
+    }
+    /// <summary>
+    /// 轉換年齡的方法
+    /// </summary>
+    public void ConverAge()
+    {
+        return age * 7;
+    }
+    /// <summary>
+    /// 狗狗吃東西的方法
+    /// </summary>
+    /// <param name="food"></param>
+    /// <param name="speed"></param>
+    public void Eat(string food,string speed = "慢")
+    {
+        Debug.Log(name + ":" + food + "，速度" + speed);
+    }
+    /// <summary>
+    /// 狗狗吃東西的方法
+    /// </summary>
+    /// <param name="food"></param>
+    /// <param name="count"></param>
+    public void Eat(string food,int count)
+    {
+        Debug.Log(name + ":" + food + "，份量:" + count);
     }
     #endregion
 
