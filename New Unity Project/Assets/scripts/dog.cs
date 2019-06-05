eeba1b81c842506ddf136cd3334cef611510a569
jack0300501 <51389225+jack0300501@users.noreply.github.com>
2019-06-05 11:41:50 +0800

    新增狗類別

---
+++ New Unity Project/Assets/scripts/dog.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class dog
+{
+    #region 欄位;
+    public string name;
+    public float weight;
+    public string type;
+    public string sex;
+    public string color;
+    public int age;
+    #endregion
+
+    #region 方法;
+    public void Bark()
+    {
+        Debug.Log(name + "旺旺!!");
+    }
+    #endregion
+
+
+    #region
+    #endregion
+}
